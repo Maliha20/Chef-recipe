da3f026781a621711192eb2e36172450ba176294
maliha20 <gulista.marium20@gmail.com>
2023-05-04 20:36:14 +0600

    about and extra sections added

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -5,8 +5,10 @@ const About = () => {
   return (
     <div className="container my-5">
       <div className="bg-color rounded p-5">
-        <h2 className="text-center font-style">Korean History and how it influenced Korean Cuisine</h2>
-        <h5 className="fs-5">
+        <h2 className="text-center font-style">
+          Korean History and how it influenced Korean Cuisine
+        </h2>
+        <h5 className="fs-5 text-white p-5 background-style">
           Korean food culture is a fascinating topic that combines the country's
           unique geography, history, and people. Korea is a peninsula located in
           Northeast Asia, bordered by China to the northwest, Russia to the
@@ -15,14 +17,32 @@ const About = () => {
           throughout history.
         </h5>
 
-        <h6>
-        The earliest known Korean cuisine dates back to the Three Kingdoms period (57 BCE-668 CE), when Korean farmers began cultivating rice, soybeans, and other crops. During the Goryeo Dynasty (918-1392 CE), Buddhism became the dominant religion, and vegetarianism spread. As a result, Korean cuisine began to incorporate a variety of vegetables, including mushrooms, roots, and greens.
-
-The Joseon Dynasty (1392-1910 CE) is considered a golden age of Korean cuisine, as it was a time of peace and prosperity. Korean food during this period was characterized by its simplicity, elegance, and use of local ingredients. Many of the dishes from this era are still enjoyed today, such as kimchi, bulgogi, and bibimbap.
-
-During the Japanese colonization of Korea (1910-1945), Korean cuisine was suppressed, and many Koreans were forced to adopt Japanese food culture. After Korea regained its independence, the country underwent rapid modernization, and Korean cuisine evolved to reflect changing tastes and lifestyles. Korean food has become increasingly popular around the world in recent years, with dishes such as Korean fried chicken and Korean barbecue gaining popularity.
-
-Today, Korean food is known for its bold flavors, extensive use of fermented ingredients, and emphasis on communal dining. Popular dishes include kimchi, a spicy fermented cabbage dish, bulgogi, a grilled beef dish, and bibimbap, a rice bowl with vegetables, meat, and egg. Korean food culture continues to evolve, reflecting the country's changing landscape and diverse population.
+        <h6 className="fs-5 text-white p-5 background-style">
+          The earliest known Korean cuisine dates back to the Three Kingdoms
+          period (57 BCE-668 CE), when Korean farmers began cultivating rice,
+          soybeans, and other crops. During the Goryeo Dynasty (918-1392 CE),
+          Buddhism became the dominant religion, and vegetarianism spread. As a
+          result, Korean cuisine began to incorporate a variety of vegetables,
+          including mushrooms, roots, and greens.
+        </h6>
+        <h6 className="fs-5 text-white p-5 background-style">
+          The Joseon Dynasty (1392-1910 CE) is considered a golden age of Korean
+          cuisine, as it was a time of peace and prosperity. Korean food during
+          this period was characterized by its simplicity, elegance, and use of
+          local ingredients. Many of the dishes from this era are still enjoyed
+          today, such as kimchi, bulgogi, and bibimbap. During the Japanese
+          colonization of Korea (1910-1945), Korean cuisine was suppressed, and
+          many Koreans were forced to adopt Japanese food culture. After Korea
+          regained its independence, the country underwent rapid modernization,
+          and Korean cuisine evolved to reflect changing tastes and lifestyles.
+          Korean food has become increasingly popular around the world in recent
+          years, with dishes such as Korean fried chicken and Korean barbecue
+          gaining popularity. Today, Korean food is known for its bold flavors,
+          extensive use of fermented ingredients, and emphasis on communal
+          dining. Popular dishes include kimchi, a spicy fermented cabbage dish,
+          bulgogi, a grilled beef dish, and bibimbap, a rice bowl with
+          vegetables, meat, and egg. Korean food culture continues to evolve,
+          reflecting the country's changing landscape and diverse population.
         </h6>
       </div>
     </div>
